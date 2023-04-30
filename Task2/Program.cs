6fd7c58f57ac002091a63e791750028ac3338106
Radmir Shagiev <veselday@gmail.com>
2023-04-30 15:06:15 +0500

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int Prompt(string msg)
+{
+
+System.Console.WriteLine($"{msg}");
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Write number, please");
+
+int fnumber(int number)
+{
+while (number > 999)
+{
+number /= 10;
+}
+return number % 10;
+}
+
+bool check(int number)
+{
+if (number < 100)
+return false;
+else return true;
+}
+
+if (check(number) != true)
+System.Console.WriteLine("The third count is not exist");
+else
+System.Console.WriteLine($"The third count of {number} is {fnumber(number)}");
