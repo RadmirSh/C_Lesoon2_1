93f84b98cbc588eb46e1c83658e7d2e074bc4cee
Radmir Shagiev <veselday@gmail.com>
2023-04-30 15:00:02 +0500

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,16 @@
-﻿
-Console.WriteLine("Hello, World!Hiudu");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Prompt (string msg)
+{
+
+System.Console.WriteLine($"{msg}");
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Write three-digit number");
+if (number >=100 && number <1000)
+{
+int mid = number/ 10 % 10;
+System.Console.WriteLine( $"Second count of {number} is {mid}");
+}
+else
+System.Console.WriteLine("It is not three-digit number");
