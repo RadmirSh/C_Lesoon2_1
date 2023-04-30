3d9a393a397532293105634064032a13ff058eb4
Radmir Shagiev <veselday@gmail.com>
2023-04-30 15:12:04 +0500

    Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int Prompt(string msg)
+{
+
+System.Console.WriteLine($"{msg}");
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Write number please");
+string ss = "It is not day of week";
+string check(int number)
+{
+if (number >= 6 && number < 8) ss = "It is Day off";
+if (number >= 1 && number < 7) ss = "It is Weekday";
+return ss;
+}
+System.Console.WriteLine(check(number));
